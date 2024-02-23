b65a53f789c5efd5909d84a68778f78aa7ea3e5e
ConKerazoE2906 <pablo.erazo01@epn.edu.ec>
2024-02-23 11:54:08 -0500

    pantalla 4

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    private static Point initialClick;
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("Base de Datos");
         frame.setContentPane(new credenciales().pantalla);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800,640);
+        frame.setSize(800, 600);
         frame.setVisible(true);
         frame.pack();
-
-
+        frame.setUndecorated(true);
+        frame.setLocationRelativeTo(null);
     }
 }
