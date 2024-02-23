7e0cf19be79c28c4658186374a000e55afa74cf4
ConKerazoE2906 <pablo.erazo01@epn.edu.ec>
2024-02-23 13:49:23 -0500

    Conexion fallida

--- src/INSERTAR.java
+++ src/INSERTAR.java
@@ -21,7 +21,7 @@ public class INSERTAR {
     public INSERTAR(){}
 
     public void ingreso_datos(int id, String nombre_apellido, String pasatiempo, String descripcion){
-        try(Connection conexion_2 = DriverManager.getConnection("jdbc:mysql://sql10.freemysqlhosting.net:3306/sql10686342", "sql10686342","FZI2EyHqCS")){
+        try(Connection conexion_2 = DriverManager.getConnection("jdbc:mysql://freemysqlhosting.net:3306/sql10686342", "sql10686342","FZI2EyHqCS")){
 
             String sql="INSERT INTO pasatiempos(id, nombre, pasatiempos, descripcion)values(?,?,?,?)";
 
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ import java.awt.event.MouseEvent;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    private static Point initialClick;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Base de Datos");
