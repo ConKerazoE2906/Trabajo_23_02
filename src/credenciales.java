6a2be04aa5034f13d0202c2825951f061145f570
ConKerazoE2906 <pablo.erazo01@epn.edu.ec>
2024-02-23 11:41:20 -0500

    l

---
+++ src/credenciales.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class credenciales {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton agragarALaBaseButton;
+
+
+
+
+}
